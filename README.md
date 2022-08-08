cf2ec069fb5441707af6a62ac828e2b4e5f7f92a
Badreddine-Ab <badrestronger@gmail.com>
2022-08-08 22:10:44 +0200

    first static page with react

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Leraning-React
-Leraning React for the first time following https://www.youtube.com/watch?v=bMknfKXIFA8&amp;t=217s course
+Leraning React for the first time following https://www.youtube.com/watch?v=bMknfKXIFA8&amp;t=217s 
---
+++ index.js
@@ -0,0 +1,21 @@
+
+const site = <div>
+    
+    
+        <img width="40px" src="React-icon.svg.png"/>
+    
+    
+        <h1>Fun facts about React</h1>
+    
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps,including mobile apps </li>
+    </ul>
+
+</div>
+ 
+ReactDOM.render(site,document.getElementById("root"))
+
